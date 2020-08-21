277e0d0eaeb583362e174b6c0b12025adf0799bb
AmeetGill <ameetpalsingh@gmail.com>
2020-08-21 09:25:16 +0530

    Delete Html.py

--- Html.py
+++
@@ -1,7 +0,0 @@
-class Html:
-	def __init__(self):
-		self.tag_string = "<html>{}</html>"
-		self.children_tag = None
-	
-	def generate_html(self):
-		print(self.tag_string.format(self.children_teg.generate()))
